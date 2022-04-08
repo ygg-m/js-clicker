6c208591850df4b2870e67f0ce04d21d1e069ef5
Pierre <pierre@scadiagro.com.br>
2022-04-08 15:55:08 -0300

    refactor some functions and fix hit animation

--- script.js
+++ script.js
@@ -9,65 +9,72 @@ var cursors = 0; // número de cursores
 var monster = 'poring'; // nome do monstro
 var monsterState = 'normal' // estado do monstro
 
-var autoPlay = false; // ativa o autoplay
 var autoPlayPower = 0;
 var autoPlayPowerCost = 50;
 
+initializer();
 
+function initializer() {
+  autoPlayInitializer();
+}
+
+function updateExp(amount) {
+    playerExp += amount;
+    updatePlayerExp();
+}
+
+function updatePlayerExp(){
+  document.getElementById("playerExp").innerHTML = playerExp;
+}
+
+function updateElementCost(elementName, cost) {
+  document.getElementById(elementName).innerHTML = cost;
+}
+
+function updateCursorCost() {
+  bonusMult += 1;
+  cursorCost = Math.round(cursorCost * 1.25);
+  totalBonus = bonusMult * bonusPlus;
+  updateElementCost("cursorCost", cursorCost);
+}
 
 function buyCursor() {
   if (playerExp >= cursorCost) {
+    updateExp(-cursorCost);
+    updateCursorCost();
+  }
+}
 
-    playerExp = playerExp - cursorCost;
-    bonusMult = bonusMult + 1;
-    cursorCost = Math.round(cursorCost * 1.25);
-    totalBonus = bonusMult * bonusPlus;
-
-    document.getElementById("playerExp").innerHTML = playerExp;
-    document.getElementById("cursorCost").innerHTML = cursorCost;
-
-  } else {}
+function updateAutoPlayCost() {
+  autoPlayPowerCost = Math.round(autoPlayPowerCost * 1.25);
+  autoPlayPower = autoPlayPower + 1;
+  updateElementCost("autoPlayPowerCost", autoPlayPowerCost);
 }
 
 function buyAutoPlay() {
   if (playerExp >= autoPlayPowerCost) {
-    if (autoPlay === false) {
-      autoPlay = true;
-
-      playerExp = playerExp - autoPlayPowerCost;
-      autoPlayPowerCost = Math.round(autoPlayPowerCost * 1.25);
-      autoPlayPower = autoPlayPower + 1;
-
-      document.getElementById("playerExp").innerHTML = playerExp;
-      document.getElementById("autoPlayButton").innerHTML = 'Power-Up Autoplay [<span id="autoPlayPowerCost">' + autoPlayPowerCost + '</span>]';
-
-      setInterval (function() {
-        playerExp = playerExp + autoPlayPower;
-        document.getElementById("playerExp").innerHTML = playerExp;
-      }, 1000); // 1000ms = 1 sec
-
-    } else {
-      playerExp = playerExp - autoPlayPowerCost;
-      autoPlayPowerCost = Math.round(autoPlayPowerCost * 1.25);
-      autoPlayPower = autoPlayPower + 1;
-
-      document.getElementById("playerExp").innerHTML = playerExp;
-      document.getElementById("autoPlayPowerCost").innerHTML = autoPlayPowerCost;
-    }
-  } else {}
+    updateExp(-autoPlayPowerCost);
+    updateAutoPlayCost();
+  }
 }
 
+function hitMonster(amount) {
+  updateExp(amount);
+  changeMonsterAnimation('hit');
 
-function addToPlayerExp(amount) {
-  playerExp = playerExp + amount;
-  monsterState = 'hit';
-  document.getElementById("monsterImg").setAttribute("src", "images/" + monster + "-" + monsterState + ".gif")
-  document.getElementById("playerExp").innerHTML = playerExp;
-
-  setTimeout(monsterAnimationNormal(), 1000);
+  setTimeout(function () {
+    changeMonsterAnimation('normal');
+  }, 100);
 }
 
-function monsterAnimationNormal() {
-  monsterState = 'normal'
-  document.getElementById("monsterImg").setAttribute("src", "images/" + monster + "-" + monsterState + ".gif")
+function changeMonsterAnimation (state) {
+  monsterState = state;
+  document.getElementById("monsterImg").setAttribute("src", "images/" + monster + "-" + state + ".gif")
+}
+
+function autoPlayInitializer () {
+  setInterval (function() {
+    playerExp += autoPlayPower;
+    updatePlayerExp();
+  }, 1000); // 1000ms = 1 sec
 }
